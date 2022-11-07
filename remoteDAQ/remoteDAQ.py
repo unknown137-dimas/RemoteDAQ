1560df58ee8120089e6c6bb8f03917249431002d
unknown137-dimas <dimasfitriokurniaw4n@gmail.com>
2022-11-07 12:47:46 +0700

    Add logging function

--- remoteDAQ/remoteDAQ.py
+++ remoteDAQ/remoteDAQ.py
@@ -17,6 +17,7 @@ from Automation.BDaq.InstantDoCtrl import InstantDoCtrl
 from Automation.BDaq.InstantAiCtrl import InstantAiCtrl
 from Automation.BDaq.InstantAoCtrl import InstantAoCtrl
 from Automation.BDaq.BDaqApi import AdxEnumToString, BioFailed
+from os import getcwd
 
 # InfluxDB connection config
 bucket = 'remote-data-acquisition'
@@ -35,8 +36,12 @@ endPort = 7
 
 # Log config
 FORMATTER = logging.Formatter('%(asctime)s — %(name)s — %(levelname)s — %(message)s')
-LOG_FILE = 'remoteDAQ.log'
 
+LOG_FILE = getcwd() + '/remoteDAQ.log'
+
+'''
+Log Function
+'''
 def get_console_handler():
    '''
    Console Output Handler
@@ -67,100 +72,104 @@ def get_logger(logger_name):
 
 my_logger = get_logger('remoteDAQ')
 
+'''
+DAQ Function
+'''
 def di_daq(devDesc, portList):
    '''
-   Function to Read Digital Signal
+   Function to Read Digital Input
    '''
+   my_logger.info('### Starting reading digital input channel data ###')
    measurement_name = 'digitalInput'
-   ret = ErrorCode.Success
-   instantDiCtrl = InstantDiCtrl(devDesc)
-   for i in portList:
-      ret, data = instantDiCtrl.readBit(0, i)
-      if BioFailed(ret):
-         break
-      yield measurement_name, i, data
-   instantDiCtrl.dispose()
+   try:
+      instantDiCtrl = InstantDiCtrl(devDesc)
+   except ValueError as e:
+      my_logger.error(e)
+   else:
+      for i in portList:
+         _, data = instantDiCtrl.readBit(0, i)
+         my_logger.info('Successfully read digital input channel # ' + str(i))
+         yield measurement_name, i, data
+      my_logger.info('### Finished reading digital input channel data ###')
+      instantDiCtrl.dispose()
    
-   if BioFailed(ret):
-      enumStr = AdxEnumToString('ErrorCode', ret.value, 256)
-      my_logger.debug('Some error occurred. And the last error code is %#x. [%s]' % (ret.value, enumStr))
-
 def do_daq(devDesc, portList, data):
    '''
-   Function to Output Digital Signal
+   Function to Write Digital Output
    '''
-   ret = ErrorCode.Success
-   instantDoCtrl = InstantDoCtrl(devDesc)
-   for i in portList:
-      ret = instantDoCtrl.writeBit(0, i, data)
-      if BioFailed(ret):
-         break
-   instantDoCtrl.dispose()
-
-   if BioFailed(ret):
-      enumStr = AdxEnumToString('ErrorCode', ret.value, 256)
-      my_logger.debug('Some error occurred. And the last error code is %#x. [%s]' % (ret.value, enumStr))
+   my_logger.info('### Starting writing digital output channel data ###')
+   try:
+      instantDoCtrl = InstantDoCtrl(devDesc)
+   except ValueError as e:
+      my_logger.error(e)
+   else:
+      for i in portList:
+         _ = instantDoCtrl.writeBit(0, i, data)
+         my_logger.info('Successfully write digital output channel # ' + str(i))
+      my_logger.info('### Finished writing digital output channel data ###')
+      instantDoCtrl.dispose()
 
 def dov_daq(devDesc, portList):
    '''
    Function to Read Digital Output Value
    '''
+   my_logger.info('### Starting reading digital output channel data ###')
    measurement_name = 'digitalOutputValue'
-   ret = ErrorCode.Success
-   instantDoCtrl = InstantDoCtrl(devDesc)
-   for i in portList:
-      ret, data = instantDoCtrl.readBit(0, i)
-      if BioFailed(ret):
-         break
-      yield measurement_name, i, data
-   instantDoCtrl.dispose()
-   
-   if BioFailed(ret):
-      enumStr = AdxEnumToString('ErrorCode', ret.value, 256)
-      my_logger.debug('Some error occurred. And the last error code is %#x. [%s]' % (ret.value, enumStr))
+   try:
+      instantDoCtrl = InstantDoCtrl(devDesc)
+   except ValueError as e:
+      my_logger.error(e)
+   else:
+      for i in portList:
+         _, data = instantDoCtrl.readBit(0, i)
+         my_logger.info('Successfully read digital output channel # ' + str(i))
+         yield measurement_name, i, data
+      my_logger.info('### Finished reading digital output channel data ###')
+      instantDoCtrl.dispose()
 
 def ai_daq(devDesc, portList, decimalPrecision=2):
    '''
    Function to Read Analog Signal
    '''
+   my_logger.info('### Starting reading analog channel data ###')
    measurement_name = 'analogInput'
-   ret = ErrorCode.Success
-   instanceAiObj = InstantAiCtrl(devDesc)
-   for i in portList:
-      ret, scaledData = instanceAiObj.readDataF64(i, 1)
-      if BioFailed(ret):
-         break
-      yield measurement_name, i, round(scaledData[0], decimalPrecision)
-   instanceAiObj.dispose()
-   
-   if BioFailed(ret):
-      enumStr = AdxEnumToString('ErrorCode', ret.value, 256)
-      my_logger.debug('Some error occurred. And the last error code is %#x. [%s]' % (ret.value, enumStr))
+   try:
+      instanceAiObj = InstantAiCtrl(devDesc)
+   # except ValueError as e:
+   except Exception as e:
+      my_logger.error(e)
+   else:
+      for i in portList:
+         _, scaledData = instanceAiObj.readDataF64(i, 1)
+         my_logger.info('Successfully read analog channel # ' + str(i))
+         yield measurement_name, i, round(scaledData[0], decimalPrecision)
+      my_logger.info('### Finished reading analog channel data ###')
+      instanceAiObj.dispose()
 
 def ao_daq(devDesc, portList, data=[0, 0]):
-    '''
-    Function to Output Analog Signal
-    '''
-    ret = ErrorCode.Success
-    instantAoCtrl = InstantAoCtrl(devDesc)
-    for i in portList:
-        ret = instantAoCtrl.writeAny(i, 1, None, data)
-        if BioFailed(ret):
-            break
-    instantAoCtrl.dispose()
-
-    if BioFailed(ret):
-        enumStr = AdxEnumToString('ErrorCode', ret.value, 256)
-        my_logger.debug('Some error occurred. And the last error code is %#x. [%s]' % (ret.value, enumStr))
-
+   '''
+   Function to Output Analog Signal
+   '''
+   my_logger.info('### Starting writing data to analog channel ###')
+   try:
+      instantAoCtrl = InstantAoCtrl(devDesc)
+   except ValueError as e:
+      my_logger.error(e)
+   else:
+      for i in portList:
+         _ = instantAoCtrl.writeAny(i, 1, None, data)
+         my_logger.info('Successfully write analog output channel # ' + str(i))
+      my_logger.info('### Finished writing data to analog channel ###')
+      instantAoCtrl.dispose()
+      
 def line_protocol(measurement_name, channel, value):
-    '''
-    Create an InfluxDB Dictionary
-    '''
-    return '{measurement},nodeID={id},channel={ch} value={val}'.format(measurement=measurement_name,
-         id=uuid3(NAMESPACE_DNS, gethostname()),
-         ch=channel,
-         val=value)
+   '''
+   Create an InfluxDB Dictionary
+   '''
+   return '{measurement},nodeID={id},channel={ch} value={val}'.format(measurement=measurement_name,
+      id=uuid3(NAMESPACE_DNS, gethostname()),
+      ch=channel,
+      val=value)
 
 def send_to_influxdb(data, num_of_points):
    '''
@@ -209,8 +218,10 @@ if __name__ == '__main__':
    # data = []
    # for i in range(num_of_points):
    while True:
-      data = [line_protocol(measurement_name=name, channel=channel, value=value) for name, channel, value in ai_daq(devDesc=devDesc, portList=[i for i in range(0,8)])]
-      print('sending data to DB...')
-      # send_to_influxdb(data, num_of_points)
-      print(data, num_of_points)
-      time.sleep(1)
+      data = [line_protocol(measurement_name=name, channel=channel, value=value) for name, channel, value in di_daq(devDesc=devDesc, portList=[i for i in range(0,8)])]
+      if data:
+         print('sending data to DB...')
+         # send_to_influxdb(data, num_of_points)
+         print(data, num_of_points)
+         time.sleep(1)
+      break
