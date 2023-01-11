313b575a8b67ecd2520131ec14bd3c786635c62c
unknown137-dimas <dimasfitriokurniaw4n@gmail.com>
2023-01-11 10:31:09 +0700

    Refactor code

--- remoteDAQ_DB_Upload.py
+++ remoteDAQ_DB_Upload.py
@@ -1,27 +1,20 @@
 
+from os import getenv
 from uuid import uuid3, NAMESPACE_DNS
 from socket import gethostname
 from influxdb_client.client.influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import ASYNCHRONOUS
-from datetime import datetime
 import remoteDAQ_Logger
 from remoteDAQ_USB import ai_daq, di_daq, doi_daq
-import asyncio
-
 
 # InfluxDB Client Config
 url='http://192.168.117.132:8086' # CHANGE THIS
-token = 'PXCJT5naU4g1R04-b9U_MtMBFLYkimD4-QZ0GyswJiBwov7MzOMj_ABppwuxvNXIK2HknQnp_qrVYXLfwY2cww=='  # CHANGE THIS
+token = str(getenv('INFLUXDB_TOKEN'))  # CHANGE THIS
 bucket = 'remote-data-acquisition' # CHANGE THIS
 org = 'UGM' # CHANGE THIS
-send_to_db = True
-
-# DAQ Config
-devDesc = 'USB-4704,BID#0' # CHANGE THIS
-portList = [_ for _ in range(0,8)]
 
 #Logger Config
-my_logger = remoteDAQ_Logger.get_logger('RemoteDAQ')
+my_logger = remoteDAQ_Logger.get_logger('RemoteDAQ_DB_Upload')
 
 # Node Config
 dev_id = str(uuid3(NAMESPACE_DNS, gethostname()))
@@ -51,51 +44,23 @@ def send_to_influxdb(
 
 '''Main Function'''
 async def main(devDesc, portList):
-   ai_results = await ai_daq(devDesc, portList)
-   di_results = await di_daq(devDesc, portList)
-   doi_results = await doi_daq(devDesc, portList)
+   daq_results = [
+         await ai_daq(devDesc, portList),
+         await di_daq(devDesc, portList),
+         await doi_daq(devDesc, portList)
+         ]
+   measurement_name = ['analogInput', 'digitalInput', 'digitalOutputValue']
    
-   ai_upload = []
-   di_upload = []
-   doi_upload = []
-   for i in portList:
-      ai_result = ai_results['data'][i]
-      di_result = di_results['data'][i]
-      doi_result = doi_results['data'][i]
-      
-      ai_tmp_upload = line_protocol(
-         measurement_name = 'analogInput',
-         id=dev_id,
-         port=ai_result['port'],
-         value=ai_result['data']
-      )
-      di_tmp_upload = line_protocol(
-         measurement_name = 'digitalInput',
-         id=dev_id,
-         port=di_result['port'],
-         value=di_result['data']
-      )
-      doi_tmp_upload = line_protocol(
-         measurement_name = 'digitalOutputValue',
-         id=dev_id,
-         port=doi_result['port'],
-         value=doi_result['data']
-      )
-      
-      ai_upload.append(ai_tmp_upload)
-      di_upload.append(di_tmp_upload)
-      doi_upload.append(doi_tmp_upload)
-   
-   send_to_influxdb(data=ai_upload)
-   send_to_influxdb(data=di_upload)
-   send_to_influxdb(data=doi_upload)
-
-if __name__ == '__main__':
-   now = datetime.now()
-   while True:
-        check = datetime.now()
-        diff = check - now
-        if diff.total_seconds() > 5:
-            print('Sending Data...')
-            asyncio.run(main(devDesc, portList))
-            now = check
+   for r in range(len(daq_results)):
+      upload = []
+      for i in portList:
+         result = daq_results[r]['data'][i]
+         tmp_upload = line_protocol(
+            measurement_name = measurement_name[r],
+            id=dev_id,
+            port=result['port'],
+            value=result['data']
+         )
+         upload.append(tmp_upload)
+      print('Sending Data to DB...')
+      send_to_influxdb(data=upload)
