23fda092f1cf4bbb36150c7e9779742d9dd09add
unknown137-dimas <dimasfitriokurniaw4n@gmail.com>
2023-01-19 11:36:53 +0700

    Update driver_install.sh

--- driver_install.sh
+++ driver_install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 apt update
-apt install -y make gcc bzip2 linux-headers-$(uname -r) kmod systemctl
+install_packages make gcc bzip2 linux-headers-$(uname -r) kmod systemctl
 mkdir -p /lib/modules/$(uname -r)/kernel/drivers/daqnavi
 mkdir -p '/usr/local/lib/x86_64-linux-gnu'
-./daq-drivers/advantech/linux_driver_source_*_64bit.run silent install usb4702_usb4704
+sudo chmod +x daq-drivers/advantech/linux_driver_source_*_64bit.run
+sudo ./daq-drivers/advantech/linux_driver_source_*_64bit.run silent install usb4702_usb4704
 apt purge -y make gcc bzip2 linux-headers-$(uname -r) kmod
 apt autoremove -y
