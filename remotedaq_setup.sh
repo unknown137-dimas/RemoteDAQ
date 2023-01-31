43794eac9a8e63724a0e78a8a0165538aec1d258
unknown137-dimas <dimasfitriokurniaw4n@gmail.com>
2023-01-31 09:21:58 +0700

    Update installer scripts

--- remotedaq_setup.sh
+++ remotedaq_setup.sh
@@ -3,7 +3,7 @@
 #User Input
 read -p 'Input wireless interface: ' wifi
 read -p 'Input Wi-Fi network name: ' ssid
-read -sp 'Input wifi password: ' pass
+read -sp 'Input Wi-Fi password: ' pass
 read -p 'Input ZeroTier network ID: ' ztid
 
 #Update Helper Scripts Permission
