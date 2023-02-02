2c650c15d365b0bc2bf9d460f61edd8281a5c627
unknown137-dimas <dimasfitriokurniaw4n@gmail.com>
2023-02-02 12:09:35 +0700

    Change variable name

--- remotedaq_setup.sh
+++ remotedaq_setup.sh
@@ -4,7 +4,7 @@
 read -p 'Input wireless interface: ' wifi
 read -p 'Input Wi-Fi network name: ' ssid
 read -sp 'Input Wi-Fi password: ' pass
-read -p 'Input ZeroTier network ID: ' ztid
+read -p 'Input ZeroTier network ID: ' zt_net_id
 
 #Update Helper Scripts Permission
 chmod +x scripts/*
@@ -12,4 +12,4 @@ chmod +x scripts/*
 #Run Helper Scripts
 ./scripts/wifi_setup.sh $wifi $ssid $pass
 ./scripts/install.sh
-./scripts/deploy.sh $ztid
+./scripts/deploy.sh $zt_net_id
