ec9db1633df167c8cea1dc98b08f94c5e573217d
unknown137-dimas <dimasfitriokurniaw4n@gmail.com>
2023-02-12 16:33:57 +0700

    Fix setup order

--- setup.sh
+++ setup.sh
@@ -8,12 +8,6 @@ read -p 'Input Wi-Fi network name: ' ssid
 read -sp 'Input Wi-Fi password: ' pass
 read -p 'Input ZeroTier network ID: ' zt_net_id
 
-# Update Helper Scripts Permission
-chmod +x scripts/*
-
-# Run Helper Scripts
-./scripts/wifi_setup.sh $wifi $ssid $pass
-
 # Install WPA Supplicant
 echo 'Installing Wi-Fi Packages...'
 sudo apt update > /dev/null 2>&1
@@ -26,9 +20,14 @@ curl -s https://install.zerotier.com | sudo bash
 echo 'Connecting to ZeroTier Network...'
 sudo zerotier-cli join $zt_net_id
 
+# Update Helper Scripts Permission
+chmod +x scripts/*
+
+# Run Helper Scripts
+./scripts/wifi_setup.sh $wifi $ssid $pass
+
 # Creating .env File
 echo HOSTNAME=$HOTSNAME >> .env
-echo ZT_ID=$(zerotier-cli info | cut -d ' ' -f 3) >> .env
 
 echo '== Init Process Completed =='
 echo '== Please Continue Setup Process from Server UI =='
