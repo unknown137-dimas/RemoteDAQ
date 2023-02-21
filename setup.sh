0a6fd27450f51b158c6ccf04e9b44f983b1e8988
unknown137-dimas <dimasfitriokurniaw4n@gmail.com>
2023-02-21 11:06:18 +0700

    Add sudoers step

--- setup.sh
+++ setup.sh
@@ -25,10 +25,15 @@ echo 'Connecting to ZeroTier Network...'
 sudo zerotier-cli join $zt_net_id
 
 # Update Helper Scripts Permission
+echo 'Configuring Wi-Fi...'
 chmod +x scripts/*
 
 # Run Helper Scripts
 ./scripts/wifi_setup.sh $wifi $ssid $pass
+
+# Set Sudoer
+echo 'Configuring Sudoers...'
+sudo echo $USER 'ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 
 echo '== Init Process Completed =='
 echo '== Please Continue Setup Process from Server UI =='
