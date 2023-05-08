581feb1d10a57930ce3b097180fdc37c68063cc3
unknown137-dimas <dimasfitriokurniaw4n@gmail.com>
2023-05-08 12:58:54 +0700

    Fix build error

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM balenalib/amd64-ubuntu-python
 ENV UDEV=1
 WORKDIR /rdaq-node
-COPY ./Automation/* /rdaq-node
-COPY ./requirements.txt /rdaq-node
+COPY . /rdaq-node
+RUN chmod +x scripts/driver_install.sh && ./scripts/driver_install.sh
 RUN pip install -r requirements.txt
 CMD python remoteDAQ.py
--- scripts/driver_install.sh
+++ scripts/driver_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo apt install -y make gcc bzip2 linux-headers-$(uname -r) kmod systemctl
+install_packages make gcc bzip2 linux-headers-$(uname -r) kmod
 sudo mkdir -p /lib/modules/$(uname -r)/kernel/drivers/daqnavi
 sudo mkdir -p '/usr/local/lib/x86_64-linux-gnu'
 sudo chmod +x daq-drivers/advantech/linux_driver_source_*_64bit.run
 sudo ./daq-drivers/advantech/linux_driver_source_*_64bit.run silent install usb4702_usb4704
-sudo apt purge -y make gcc bzip2 kmod
+sudo apt purge -y make gcc bzip2 linux-headers-$(uname -r)
 sudo apt autoremove -y
