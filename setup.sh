0a2a8a3c9806e93d81425cada564e18ce60d2549
unknown137-dimas <dimasfitriokurniaw4n@gmail.com>
2023-05-09 10:25:32 +0700

    Add script option

--- setup.sh
+++ setup.sh
@@ -3,30 +3,41 @@
 # Intro
 echo '== Node Init Process Begin =='
 
-# User Input
-read -p 'Input wireless interface: ' wifi
-read -p 'Input Wi-Fi network name: ' ssid
-read -sp 'Input Wi-Fi password: ' pass
-read -p 'Input ZeroTier network ID: ' zt_net_id
+# Script Options
+wireless='false'
 
-# Install WPA Supplicant
-echo 'Installing Wi-Fi Packages...'
-sudo apt update > /dev/null 2>&1
-sudo apt install -y wpasupplicant > /dev/null 2>&1
+while getopts 'w' flag; do
+    case "${flag}" in
+        w) wireless='true' ;;
+        *) wireless='false' ;;
+    esac
+done
+
+# User Input
+if $wireless; then
+    read -p 'Input wireless interface: ' wifi
+    read -p 'Input Wi-Fi network name: ' ssid
+    read -sp 'Input Wi-Fi password: ' pass
+fi
+read -p 'Input ZeroTier network ID: ' zt_net_id
 
 # Install Ansible
 echo 'Installing Ansible...'
 sudo apt install -y ansible > /dev/null 2>&1
 
-# Update Helper Scripts Permission
-echo 'Configuring Wi-Fi...'
-chmod +x scripts/*
+if $wireless; then
+    # Install WPA Supplicant
+    echo 'Installing Wi-Fi Packages...'
+    sudo apt update > /dev/null 2>&1
+    sudo apt install -y wpasupplicant > /dev/null 2>&1
 
-# Setup Wi-Fi Network
-sudo ./scripts/wifi_setup.sh $wifi $ssid $pass
+    # Update Helper Scripts Permission
+    echo 'Configuring Wi-Fi...'
+    chmod +x scripts/*
 
-# Install DAQ Driver
-sudo ./scripts/driver_install.sh
+    # Setup Wi-Fi Network
+    sudo ./scripts/wifi_setup.sh $wifi $ssid $pass
+fi
 
 # Install Zerotier Client
 curl -s https://install.zerotier.com | sudo bash
