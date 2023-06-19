47d4601516c528ae109339f5baab99a0058b4759
Dimas Fitrio Kurniawan <dimasfitriokurniaw4n@gmail.com>
2023-06-19 14:57:06 +0700

    Update DAQ Driver Script

--- scripts/driver_install.sh
+++ scripts/driver_install.sh
@@ -3,6 +3,6 @@ install_packages make gcc bzip2 linux-headers-$(uname -r) kmod
 sudo mkdir -p /lib/modules/$(uname -r)/kernel/drivers/daqnavi
 sudo mkdir -p '/usr/local/lib/x86_64-linux-gnu'
 sudo chmod +x daq-drivers/advantech/linux_driver_source_*_64bit.run
-sudo ./daq-drivers/advantech/linux_driver_source_*_64bit.run silent install usb4702_usb4704
+sudo ./daq-drivers/advantech/*_64bit.run silent install usb4702_usb4704
 sudo apt purge -y make gcc bzip2 linux-headers-$(uname -r)
 sudo apt autoremove -y
